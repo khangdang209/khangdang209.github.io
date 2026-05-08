4ad6892c6479e07270f09008f69b3cdb96e48477
Thomas Khang <dangchankhang@gmail.com>
2026-05-08 12:25:29 +0700

    update GITHUB_USERNAME constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
 export const GLOBAL_ENV = import.meta.env;
 
-// export const GITHUB_USERNAME = window.location.hostname.split(".")[0];
-export const GITHUB_USERNAME = "cyplucastero.github.io".split(".")[0];
+export const GITHUB_USERNAME = window.location.hostname.split(".")[0];
